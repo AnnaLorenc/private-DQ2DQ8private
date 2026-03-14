20d31242f3b79fb30b1bd54a2e276a6ae72a9e18
Anna Lorenc <aniprywatne@gmail.com>
2026-03-14 21:57:01 +0100

    started rarefied sharing plots

--- for_plots_R/paper_figures2026C.R
+++ for_plots_R/paper_figures2026C.R
@@ -198,3 +198,104 @@ p+ stat_compare_means(
 ) +
   # Add an overall ANOVA/Kruskal-Wallis test at the top
   stat_compare_means(label.y = max(plot_data$jaccard) * 1.5) 
+
+
+
+
+################
+##
+## PUBLIC SEQUENCES
+##
+################
+
+##Improve the main title part
+##add N of clonotypes in the 'leading' column
+##overpotting issue???
+
+
+plot_sharing <- function(sharing_summary, cfs, min_frac, clonotype_cols=c(  "aminoAcid" ,  "vFamilyName" ,"jGeneName"), for_title=""){
+  max_sharing <- colnames(sharing_summary) %>%
+    grep(pat="^X", val=T) %>%
+    gsub(pat='X', rep='') %>%
+    as.integer(.)%>%
+    max()
+  max_shared_within_geno <- sapply(c("DQ8", "DQ2", "hete"), function(grp)
+    sharing_summary%>%
+      filter(.data[[paste0("X", cfs)]] > 0, group == grp)%>%
+      select(-c(paste0('X',c(3:max_sharing)[3:max_sharing <cfs])))%>%
+      pivot_longer(cols=starts_with('X'), names_to="shared_by")%>%
+      filter(value>= min_frac)%>%
+      mutate(shared=as.integer(gsub(pat='X', rep='',shared_by)))%>%
+      unite("clonotype", all_of(clonotype_cols), sep = "_", remove = FALSE)%>%
+      group_by(clonotype)%>%
+      slice_max(shared), USE.NAMES = TRUE, simplify=FALSE
+  )
+  
+  top_hits <- sapply(max_shared_within_geno, function(mswg)
+    left_join(mswg%>%
+                select(clonotype),
+              sharing_summary%>%pivot_longer(cols=starts_with('X'), names_to="shared_by")%>%
+                filter(value>= min_frac)%>%
+                mutate(shared=as.integer(gsub(pat='X', rep='',shared_by)))%>%
+                select(-c(shared_by, value))%>%
+                unite("clonotype", all_of(clonotype_cols), sep = "_", remove = FALSE)%>%
+                group_by(across(all_of(c("group", "clonotype"))))%>%
+                slice_max(shared),
+              by ="clonotype")%>%
+      pivot_wider( names_from = group,values_from = shared, values_fn=max,values_fill = 0),
+    USE.NAMES = TRUE, simplify=FALSE)
+  
+  #now visualise top hits
+  plot_one_panel <- function(panel_df, name,clonotype_cols){
+    p <- panel_df%>%
+      pivot_longer(c(DQ2,hete,DQ8), names_to = "geno")%>%
+      mutate(geno=factor(geno, levels=c("DQ2", "hete", "DQ8")))%>%
+      ggplot(aes(x=geno, y=value, group=clonotype, col=clonotype)) +geom_line() +theme_bw()+
+      scale_y_discrete()+ylim(0, max_sharing)+
+      guides(color="none")+
+      ggtitle(name)
+    p
+  }
+  library(patchwork)
+  
+  all3plots <- lapply(c("DQ2", "hete", "DQ8"), function(i)plot_one_panel (top_hits[[i]], i,clonotype_cols=clonotype_cols))
+  
+  p <- wrap_plots(all3plots, ncol=3)
+  p+ plot_annotation(title = paste("clonotype by", paste0(sapply(clonotype_cols, substr,start=1, stop=4),collapse="_"),"min.frac=",min_frac, " cfs=",cfs," ", for_title))
+}
+
+
+
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid_vFamilyName_jGeneName/sharing_summary_N_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+plot_sharing (sharing_summary, cfs=7, min_frac=20, clonotype_cols=c(  "aminoAcid" ,  "vFamilyName" ,"jGeneName"), for_title=" naive cells")
+
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid_vFamilyName/sharing_summary_N_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+
+plot_sharing (sharing_summary, cfs=7, min_frac=20, clonotype_cols=c(  "aminoAcid" ,  "vFamilyName"), for_title=" naive cells")
+
+
+##something wrong when just aminoacid column
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid/sharing_summary_N_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+
+plot_sharing (sharing_summary, cfs=7, min_frac=20, clonotype_cols=c(  "aminoAcid" ), for_title=" naive cells")
+
+
+
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid_vFamilyName_jGeneName/sharing_summary_E_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+plot_sharing (sharing_summary, cfs=7, min_frac=20, clonotype_cols=c(  "aminoAcid" ,  "vFamilyName" ,"jGeneName"), for_title=" memory cells")
+
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid_vFamilyName/sharing_summary_E_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+
+plot_sharing (sharing_summary, cfs=7, min_frac=20, clonotype_cols=c(  "aminoAcid" ,  "vFamilyName"), for_title=" memory cells")
+
+
+##something wrong when just aminoacid column
+sharing_summary <- read_tsv("/Users/ania/Documents/DQ2DQ8/pipeline/DQ2DQ8/results/rarified_sharing/aminoAcid/sharing_summary_E_A3/merged_summary.tsv")
+names(sharing_summary) <- make.names(names(sharing_summary))
+
+plot_sharing (sharing_summary, cfs=7, min_frac=10, clonotype_cols=c(  "aminoAcid" ), for_title=" memory cells")
